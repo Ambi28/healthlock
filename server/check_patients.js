08991d53f7173e8f500511db2860b4c2e972e458
Ambi28 <ambica2805@gmail.com>
2026-04-19 18:05:52 +0530

    Update check_patients.js

--- server/check_patients.js
+++ server/check_patients.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/health').then(async () => {
+mongoose.connect('mongodb+srv://ambica2805_db_user:<7rBMWG8kQRL22miL>@cluster0.ihmdbz9.mongodb.net/?appName=Cluster0').then(async () => {
     const db = mongoose.connection.db;
     const patients = await db.collection('patients').find().toArray();
-    console.log(JSON.stringify(patients.map(p => ({ 
-        patientId: p.patientId, 
-        name: p.name, 
+    console.log(JSON.stringify(patients.map(p => ({
+        patientId: p.patientId,
+        name: p.name,
         qrToken: p.qrToken,
-        historyCount: (p.tokenHistory || []).length 
+        historyCount: (p.tokenHistory || []).length
     })), null, 2));
     process.exit(0);
 });
